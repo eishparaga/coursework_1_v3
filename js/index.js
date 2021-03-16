2d227f10f58a6cd0163f5177ba0e012c79311e20
Evgeny Shparaga <eishparaga@yandex.ru>
2021-03-16 23:16:02 +0300

    1 parts: markup, 1920, style

--- js/index.js
+++ js/index.js
@@ -1,3 +1,47 @@
+// Кастомный Scroll
+
+Array.prototype.forEach.call(    
+  document.querySelectorAll('.droplist__list'),    
+  el => new SimpleBar(el, {
+      autoHide: false,
+      scrollbarMaxSize: 28
+    }      
+  )  
+);
+
+
+// ------
+// Выпадающее меню
+
+document.addEventListener('DOMContentLoaded', function() {
+  let trendList = document.querySelector('.trend-list');
+  let trendItems = trendList.querySelectorAll('.trend-items');
+  let trendItemBtn = trendList.querySelectorAll('.trend-item__btn');
+
+  function toggleMenu() {
+      let thisItem = this.parentNode;
+      
+
+      trendItems.forEach(item => {
+          if (thisItem == item) {
+              thisItem.classList.toggle('active');
+              return;
+          }
+          item.classList.remove('active');
+      });
+  }
+
+  trendItemBtn.forEach(question => question.addEventListener('click', toggleMenu));
+
+});
+
+// Клик мимо
+    $(document.body).on('click', function (click) {
+      if (!click.target.classList.contains('trend-item__btn')) {
+        $('.trend-items').removeClass('active');
+      }
+    });
+
 //-------
 // Кастомный Select
 //-------
@@ -16,14 +60,15 @@ castomSelect();
 // Swiper Gallery
 //-------
 
-const swiper = new Swiper('.gallery__right .swiper-container', {
+const swiperGallery = new Swiper('.gallery__right .swiper-container', {
   // Optional parameters
   slidesPerView: 3,
   slidesPerColumn: 2,
   autoHeight: false,
   spaceBetween: 50,
   slidesPerGroup: 3,
-  
+  loop: false,
+
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
@@ -41,13 +86,14 @@ const swiper = new Swiper('.gallery__right .swiper-container', {
 // Swiper Publication
 //-------
 
-const swiperSecond = new Swiper('.publication__right-side .swiper-container', {
+const swiperPublic = new Swiper('.publication__right-side .swiper-container', {
 // Optional parameters
   slidesPerView: 3,
   slidesPerColumn: 1,
   autoHeight: false,
   spaceBetween: 50,
-  slidesPerGroup: 2,
+  slidesPerGroup: 3,
+  
   
   // If we need pagination
   pagination: {
@@ -61,6 +107,26 @@ const swiperSecond = new Swiper('.publication__right-side .swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+//-------
+// Swiper Project
+//-------
+
+const swiperProject = new Swiper('.project__swiper .swiper-container', {
+  // Optional parameters
+    slidesPerView: 3,
+    slidesPerColumn: 1,
+    autoHeight: false,
+    spaceBetween: 50,
+    slidesPerGroup: 3,
+      
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
 
 //-------
 // TABS
@@ -160,3 +226,40 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('events-btn').classList.add('disable');
   };
 });
+
+
+//-------
+// MAP
+//-------
+
+// Функция ymaps.ready() будет вызвана, когда
+// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        const myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+            // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.760787, 37.636402],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 14
+        });
+        myPlacemark = new ymaps.Placemark([55.758438, 37.601088], {}, {
+          iconLayout: 'default#image',
+          iconImageHref: '/img/contacts/map-point.svg',
+          iconImageSize: [20, 20],
+          iconImageOffset: [-10, -10]
+        }),
+        myMap.geoObjects.add(myPlacemark);
+        myMap.controls.remove('zoomControl',);
+        myMap.controls.remove('geolocationControl');
+        myMap.controls.remove('trafficControl');
+        myMap.controls.remove('routeButtonControl');
+        myMap.controls.remove('searchControl');
+        myMap.controls.remove('typeSelector');
+        myMap.controls.remove('fullscreenControl');
+        myMap.controls.remove('rulerControl');
+    }
